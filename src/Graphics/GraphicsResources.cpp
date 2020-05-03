0238597c41da41561fc7933755f145e5d55eba7b
Viacheslav Tekaev <tekaeff@yandex.ru>
2020-05-03 15:02:32 +0300

    Add post build step to move data folder into project's bin folder.

--- src/Graphics/GraphicsResources.cpp
+++ src/Graphics/GraphicsResources.cpp
@@ -2,7 +2,7 @@
 
 namespace GraphicsResources
 {
-	const char* const shadersFolderPath{ R"(../data/Shaders/)" };
-	const char* const texturesFolderPath{ R"(../data/Textures/)" };
-    const char* const fonstFolderPath{ R"(../data/Fonts/)" };
+    const char* const shadersFolderPath{ R"(data/Shaders/)" };
+    const char* const texturesFolderPath{ R"(data/Textures/)" };
+    const char* const fonstFolderPath{ R"(data/Fonts/)" };
 }
